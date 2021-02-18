ed4fbccad3ca3bd1b5f51ba8554d38f6a51b54c4
hwee1115 <hwee1115@naver.com>
2021-02-18 17:12:54 +0900

    test

--- src/Team1/KimMyungHwi.java
+++ src/Team1/KimMyungHwi.java
@@ -1,10 +1,26 @@
 package Team1;
 
+import java.util.Scanner;
+
 public class KimMyungHwi {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		boolean run =true;
+		String [][] boardArray = new String[100][5];
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		while(run) {
+			System.out.println("--------------------------");
+			System.out.println("1.목록 | 2.생성(Create) | 3.읽기(Read) | 4.수정(Update) | 5.삭제(Delete) | 6.종료");
+			System.out.println("--------------------------");
+			System.out.print("메뉴선택> ");
+			int choiceNum =Integer.parseInt(scanner.nextLine());
+			if(choiceNum==1) {
+				System.out.println("--------------------------");
+			}
+		}
 	}
 
 }
